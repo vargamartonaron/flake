8362adf9358a8c3d45280cd10b297ef207f8bac4
Marton A. Varga <martonaronvarga@metasciencelab.hu>
2024-11-05 22:23:08 +0100

    fix

--- home/services/wayland/dunst.nix
+++ home/services/wayland/dunst.nix
@@ -2,56 +2,60 @@
   services.dunst = {
     enable = true;
     settings = {
-      font = "Terminus Font 12";
-      allow_markup = "yes";
-      format = "<b>%s</b>\n%b";
-      sort = "yes";
-      indicate_hidden = "yes";
-      alignment = "left";
-      bounce_freq = 0;
-      ellipsize = "middle";
-      show_age_threshold = -1;
-      word_wrap = "yes";
-      ignore_newline = "no";
+      global = {
+        font = "Terminus Font 12";
+        allow_markup = "yes";
+        format = "<b>%s</b>\n%b";
+        sort = "yes";
+        indicate_hidden = "yes";
+        alignment = "left";
+        bounce_freq = 0;
+        ellipsize = "middle";
+        show_age_threshold = -1;
+        word_wrap = "yes";
+        ignore_newline = "no";
 
-      width = 320;
-      heigth = 500;
-      origin = "top_right";
-      offet = "10x12";
-      progress_bar = true;
-      progress_bar_heigth = 14;
-      progress_bar_frame_wifth = 1;
-      progress_bar_min_width = 150;
-      progress_bar_max_width = 300;
+        width = 320;
+        heigth = 500;
+        origin = "top_right";
+        offet = "10x12";
+        progress_bar = true;
+        progress_bar_heigth = 14;
+        progress_bar_frame_wifth = 1;
+        progress_bar_min_width = 150;
+        progress_bar_max_width = 300;
 
-      frame_width = 3;
-      frame_color = "#000000";
-      transparency = 0;
-      idle_threshold = 0;
-      monitor = 0;
-      follow = "keyboard";
-      show_indicators = "no";
-      sticky_history = "yes";
-      line_height = 0;
-      separator_height = 3;
-      padding = 16;
-      horizontal_padding = 12;
+        frame_width = 3;
+        frame_color = "#000000";
+        transparency = 0;
+        idle_threshold = 0;
+        monitor = 0;
+        follow = "keyboard";
+        show_indicators = "no";
+        sticky_history = "yes";
+        line_height = 0;
+        separator_height = 3;
+        padding = 16;
+        horizontal_padding = 12;
 
-      text_icon_padding = 16;
-      separator_color = "frame";
+        text_icon_padding = 16;
+        separator_color = "frame";
 
-      startup_notification = "no";
-      icon_position = "left";
+        startup_notification = "no";
+        icon_position = "left";
 
-      min_icon_size = 32;
-      max_icon_size = 48;
+        min_icon_size = 32;
+        max_icon_size = 48;
 
-      corner_radius = 15;
-      always_run_script = "true";
+        corner_radius = 15;
+        always_run_script = "true";
+      };
 
-      mouse_left_click = "close_current";
-      mouse_middle_click = "do_action, close_current";
-      mouse_right_click = "close_all";
+      shortcuts = {
+        mouse_left_click = "close_current";
+        mouse_middle_click = "do_action, close_current";
+        mouse_right_click = "close_all";
+      };
 
       urgency_low = {
         timeout = 6;
