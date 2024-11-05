b1e10188f85fc7a69496c2f7190697f35108e410
Marton A. Varga <martonaronvarga@metasciencelab.hu>
2024-11-05 22:44:25 +0100

    --amend

--- home/programs/media/default.nix
+++ home/programs/media/default.nix
@@ -1,10 +1,7 @@
-{pkgs, lib, ...}:
+{pkgs, ...}:
 # media - control and enjoy audio/video
 {
-  pkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
-    "spotify"
-  ];
-
+ 
   home.packages = with pkgs; [
     # audio control
     pulsemixer
