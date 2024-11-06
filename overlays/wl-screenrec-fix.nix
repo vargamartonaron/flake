fd59381d760baf19c75d2ffd94153deb8440f2df
Marton A. Varga <martonaronvarga@metasciencelab.hu>
2024-11-06 10:00:38 +0100

    debug

--- overlays/wl-screenrec-fix.nix
+++ overlays/wl-screenrec-fix.nix
@@ -1,14 +1,12 @@
-{ lib, ...}: 
 
-let
-  wlScreenrecFix = final: prev: {
+final: prev: {
     wl-screenrec = prev.wl-screenrec.overrideAttrs (oldAttrs: {
+      # Use the specific commit from Nixpkgs
       src = prev.fetchFromGitHub {
         owner = "NixOS";
         repo = "nixpkgs";
-        rev = "a36b4d9ac9e2f4c42b145685a31ac8b58776e8cb";
+        rev = "a36b4d9ac9e2f4c42b145685a31ac8b58776e8cb";  # The commit hash
       };
     });
-  };
-in
-[ wlScreenrecFix ]
+}
+
