e87fe0dffad5dc1612881d74ef3dcb1f4b3a6519
Marton A. Varga <martonaronvarga@metasciencelab.hu>
2024-11-06 12:05:12 +0100

    pinning ffmpeg 6 to wl-screenrec

--- overlays/wl-screenrec-fix.nix
+++ overlays/wl-screenrec-fix.nix
@@ -1,13 +1,6 @@
-
-final: prev: {
-    wl-screenrec = prev.wl-screenrec.overrideAttrs (oldAttrs: {
-      # Use the specific commit from Nixpkgs
-      src = prev.fetchFromGitHub {
-        owner = "NixOS";
-        repo = "nixpkgs";
-        rev = "a36b4d9ac9e2f4c42b145685a31ac8b58776e8cb";
-        hash = "sha256-1m4ic7f0l27pb6g0lfqg2vs8955202zrjl96pcnm4nbmnmcjr8rs";
+self: super: {
+    wl-screenrec = super.wl-screenrec.override {
+      ffmpeg = super.ffmpeg_6;
       };
-    });
 }
 
