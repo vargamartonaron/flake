9841bcd161e02d2059f5a22557dd6e9f5d009de7
Marton A. Varga <martonaronvarga@metasciencelab.hu>
2024-11-06 09:51:06 +0100

    debug

--- overlays/wl-screenrec-fix.nix
+++ overlays/wl-screenrec-fix.nix
@@ -1,9 +1,9 @@
-{ pkgs ? import <nixpkgs> {}}:
+{ lib, ...}: 
 
 let
   wlScreenrecFix = final: prev: {
     wl-screenrec = prev.wl-screenrec.overrideAttrs (oldAttrs: {
-      src = pkgs.fetchFromGitHub {
+      src = prev.fetchFromGitHub {
         owner = "NixOS";
         repo = "nixpkgs";
         rev = "a36b4d9ac9e2f4c42b145685a31ac8b58776e8cb";
