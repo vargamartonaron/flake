1f39192fa670f393134233a54723fe49677e91b7
Marton A. Varga <martonaronvarga@metasciencelab.hu>
2024-11-06 10:02:21 +0100

    fix sha

--- overlays/wl-screenrec-fix.nix
+++ overlays/wl-screenrec-fix.nix
@@ -5,7 +5,7 @@ final: prev: {
       src = prev.fetchFromGitHub {
         owner = "NixOS";
         repo = "nixpkgs";
-        rev = "a36b4d9ac9e2f4c42b145685a31ac8b58776e8cb";  # The commit hash
+        rev = "sha256-a36b4d9ac9e2f4c42b145685a31ac8b58776e8cb";  # The commit hash
       };
     });
 }
