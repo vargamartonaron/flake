4ca6ad33eaeabf74fab27c6a727d2817036531b5
Marton A. Varga <martonaronvarga@metasciencelab.hu>
2025-05-04 14:27:49 +0200

    updates

--- home/terminal/programs/btop.nix
+++ home/terminal/programs/btop.nix
@@ -7,7 +7,7 @@
   xdg.configFile = {
     "btop/themes/greyscale.theme".source = pkgs.fetchurl {
       url = "https://github.com/aristocratos/btop/blob/bfe8c20ebe3d180f7e0b6b2ac016c749ad3a996a/themes/greyscale.theme";
-      hash = "sha256-YjRDJShAdVYoRreCeCMA80HhfsFOaGYlop/LoY3Kvuo=";
+      hash = "sha256-Uk0Vpt+gyZ6nhrS1xd2UQdsAwzg427GmKkPwyoB+M5o=";
     };
   };
 }
